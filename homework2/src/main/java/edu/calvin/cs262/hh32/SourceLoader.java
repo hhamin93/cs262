858700088c095c097279f9ffb7efcc73fe32e6a2
Hamin Hong <hh32@students.calvin.edu>
2019-10-18 20:09:41 -0400

    done

---
+++ homework2/src/main/java/edu/calvin/cs262/hh32/SourceLoader.java
@@ -0,0 +1,34 @@
+package edu.calvin.cs262.hh32;
+
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.loader.content.AsyncTaskLoader;
+
+public class SourceLoader extends AsyncTaskLoader<String> {
+
+        private String mQueryString;
+        private String mTransferProtocol;
+        Context mContext;
+
+        public SourceLoader(@NonNull Context context, String queryString, String transferProtocol) {
+            super(context);
+            mContext = context;
+            mQueryString = queryString;
+            mTransferProtocol = transferProtocol;
+        }
+
+        @Nullable
+        @Override
+        public String loadInBackground() {
+            return NetworkUtils.getSourceCode(mContext, mQueryString, mTransferProtocol);
+        }
+
+        @Override
+        protected void onStartLoading() {
+            super.onStartLoading();
+            forceLoad();
+        }
+}
